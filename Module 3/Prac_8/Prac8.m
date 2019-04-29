b8094acfd64f896e226ad2906beb3ffe97e9b08c
MatthewWhitfield <42330971+MatthewWhitfield@users.noreply.github.com>
2019-04-29 13:16:44 +1000

    Update Prac8.m

--- Module 3/Prac_8/Prac8.m
+++ Module 3/Prac_8/Prac8.m
@@ -4,35 +4,35 @@
 addpath('Beacon_images/Beacon_images')
 load('Arena_img.mat')
 load('imgall3_3.mat')
-pb = PiBot('172.19.232.178','172.19.232.12',6);
+pb = PiBot('172.19.232.173');
 Beacon = [];
 char ID;
 
 img = imread('GetImage.png');
-%cam = pb.getImage;
+cam = pb.getImage;
 
-[r,g,b] = chromaticity(img);
+[r,g,b] = chromaticity(cam);
 
-[Rblobs, Yblobs, Bblobs, imgRblobs, imgYblobs, imgBblobs] = getBlobs(img);
+[Rblobs, Yblobs, Bblobs, imgRblobs, imgYblobs, imgBblobs] = getBlobs(cam);
 
 %Sorts the blobs into uc values
 for i = 1:length(Rblobs)
     for j = 1:length(Rblobs)
-        if Rblobs(i).uc < Rblobs(j).uc
+        if Rblobs(i).uc > Rblobs(j).uc
             temp = Rblobs(i);
             Rblobs(i) = Rblobs(j);
             Rblobs(j) = temp;
             
         end
         
-        if Yblobs(i).uc < Yblobs(j).uc
+        if Yblobs(i).uc > Yblobs(j).uc
             temp = Yblobs(i);
             Yblobs(i) = Yblobs(j);
             Yblobs(j) = temp;
             disp('Hey')
         end
         
-        if Bblobs(i).uc < Bblobs(j).uc
+        if Bblobs(i).uc > Bblobs(j).uc
             temp = Bblobs(i);
             Bblobs(i) = Bblobs(j);
             Bblobs(j) = temp;
@@ -42,31 +42,30 @@ end
 
 for i = 1:length(Rblobs)
     %Returns the beacon ID
-    if Rblobs(i).vc < Yblobs(i).vc && Rblobs(i).vc < Bblobs(i).vc && Yblobs(i).vc < Bblobs(i).vc
+    if Rblobs(i).vc > Yblobs(i).vc && Rblobs(i).vc > Bblobs(i).vc && Yblobs(i).vc > Bblobs(i).vc
         ID = '011110';
         ID = bin2dec(ID);
-        disp('1')
-    elseif Rblobs(i).vc < Yblobs(i).vc && Rblobs(i).vc < Bblobs(i).vc && Bblobs(i).vc < Yblobs(i).vc
+        disp(ID)
+    elseif Rblobs(i).vc > Yblobs(i).vc && Rblobs(i).vc > Bblobs(i).vc && Bblobs(i).vc > Yblobs(i).vc
         ID = '011011';
-        disp('2')
         ID = bin2dec(ID);
-
-    elseif Bblobs(i).vc < Yblobs(i).vc && Bblobs(i).vc < Rblobs(i).vc && Yblobs(i).vc < Rblobs(i).vc
+        disp(ID)
+    elseif Bblobs(i).vc > Yblobs(i).vc && Bblobs(i).vc > Rblobs(i).vc && Yblobs(i).vc > Rblobs(i).vc
         ID = '101101';
         ID = bin2dec(ID);  
-        disp('3')
-    elseif Bblobs(i).vc < Yblobs(i).vc && Bblobs(i).vc < Rblobs(i).vc && Rblobs(i).vc < Yblobs(i).vc
+        disp(ID)
+    elseif Bblobs(i).vc > Yblobs(i).vc && Bblobs(i).vc > Rblobs(i).vc && Rblobs(i).vc > Yblobs(i).vc
         ID = '100111';
         ID = bin2dec(ID);
-        disp('4')
-    elseif Yblobs(i).vc < Bblobs(i).vc && Yblobs(i).vc < Rblobs(i).vc && Bblobs(i).vc < Rblobs(i).vc
+        disp(ID)
+    elseif Yblobs(i).vc > Bblobs(i).vc && Yblobs(i).vc > Rblobs(i).vc && Bblobs(i).vc > Rblobs(i).vc
         ID = '111001';
         ID = bin2dec(ID);
-        disp('5')
-    elseif Yblobs(i).vc < Bblobs(i).vc && Yblobs(i).vc < Rblobs(i).vc && Rblobs(i).vc < Bblobs(i).vc
+        disp(ID)
+    elseif Yblobs(i).vc > Bblobs(i).vc && Yblobs(i).vc > Rblobs(i).vc && Rblobs(i).vc > Bblobs(i).vc
         ID = '110110';
         ID = bin2dec(ID);
-        disp('6')
+        disp(ID)
     end
 end
 
