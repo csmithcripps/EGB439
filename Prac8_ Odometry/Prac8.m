00fa5e5a28f9de38184d65127549dd80d20bda48
csmithcripps <codysmithcripps@outlook.com>
2019-04-29 16:47:30 +1000

    Prac8 done Prac9 progress

--- Prac8_ Odometry/Prac8.m
+++ Prac8_ Odometry/Prac8.m
@@ -7,8 +7,8 @@ addpath('../Robot_Functions')
 pb = PiBot('172.19.232.173');
 
 % Initial values/constants
-q = [0 0 0];
-goal = [0.5 0.5];
+q = [0.1 0.1 0];
+goal = [1 1];
 
 % Setup values
 qList = q;
@@ -19,7 +19,7 @@ pb.resetEncoder
 figure
 hold on
 r = sqrt(0.5^2 + 0.5^2);
-while (r > 0.1)    
+while (r > 0.3)    
     %% Estimate Position   
     % Update Encoder
     encoder = pb.getEncoder;
@@ -28,7 +28,7 @@ while (r > 0.1)
     
     %% Control Robot
     % Drive toward goal
-    vel = control.driveToPoint(q,goal,0.23,0.15);  % compute the wheel speeds given the current configuration
+    vel = control.driveToPoint(q,goal,0.3,0.04);  % compute the wheel speeds given the current configuration
     pb.setVelocity(vel)
     
     % Update values
@@ -46,15 +46,33 @@ while (r > 0.1)
         drawnow
     end
     axis equal
+    hold on 
+    plot(goal(1),goal(2), 'bo')
     drawnow
     
     r = sqrt((goal(1) - q(1))^2 + (goal(2) - q(2))^2);
+    if r < 0.4       
+        cam = pb.getImage();
+        idList = idBeacon(cam);
+        if ~isempty(idList)
+            idList
+        end
+        disp(idList)
+    end
+        
     pause(0.1);   
+end
+pb.stop
+
+%% Look for Beacons
+cam = pb.getImage();
+idList = idBeacon(cam);
+if ~isempty(idList)
+    idList
 end
 
 
 %% End Program
-pb.stop
 disp('Press <Enter> to exit')
 pause
 close all
